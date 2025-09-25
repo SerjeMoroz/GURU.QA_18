8d2a273b52ef9e83ac47b4d4664a0953e9a7796a
Sergei Moroz <moroz.sergei94@gmail.com>
2025-09-25 16:21:52 +0300

    add test

--- src/test/java/tests/firstTest.java
+++ src/test/java/tests/firstTest.java
@@ -2,6 +2,9 @@ package tests;
 
 import examples.BaseTest;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import java.io.File;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -22,7 +25,13 @@ public class firstTest extends BaseTest {
         $(".react-datepicker__year-select").selectOption("2000");
         $(".react-datepicker__day--016:not(react-datepicker__day--weekend)").click();
         $("label[for='hobbies-checkbox-3']").click();
-
+        $("#subjectsInput").setValue("English").pressEnter();
+//        $("#uploadPicture").uploadFile(new File("BlueBird.jpg"));
         $("#currentAddress").setValue("Пушкина колотушкина 54");
+        $("#state").click();
+        $("#react-select-3-option-0").click();
+        $("#city").click();
+        $("#react-select-4-option-1").click();
+        $("#submit").click();
     }
 }
