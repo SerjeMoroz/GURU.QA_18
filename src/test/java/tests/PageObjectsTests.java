d3b7395a7cf70976518a06b7e204d16884b25a7f
ALBERICH <Moroz.sergei94@gmail.com>
2025-11-17 23:14:14 +0300

    update faker

--- src/test/java/tests/PageObjectsTests.java
+++ src/test/java/tests/PageObjectsTests.java
@@ -44,16 +44,6 @@ public class PageObjectsTests extends BaseTest {
                             .verifyModalWindowResult("Picture", "BlueBird.jpg")
                             .verifyModalWindowResult("State and City", userState() + userCity());
         registrationResultModal.closeModalWindow();
-
-//        $(By.xpath("//td[normalize-space()='Student Name']/following-sibling::td[normalize-space()='Serje Moroz']"));
-//        $(By.xpath("//td[normalize-space()='Student Email']/following-sibling::td[normalize-space()='SerjeMoroz@gmail.com']"));
-//        $(By.xpath("//td[normalize-space()='Gender']/following-sibling::td[normalize-space()='Male']"));
-//        $(By.xpath("//td[normalize-space()='Mobile']/following-sibling::td[normalize-space()='0123456789']"));
-//        $(By.xpath("//td[normalize-space()='Date of Birth']/following-sibling::td[normalize-space()='17 August,2000']"));
-//        $(By.xpath("//td[normalize-space()='Subjects']/following-sibling::td[normalize-space()='English']"));
-//        $(By.xpath("//td[normalize-space()='Hobbies']/following-sibling::td[normalize-space()='Music']"));
-//        $(By.xpath("//td[normalize-space()='Picture']/following-sibling::td[normalize-space()='BlueBird.jpg']"));
-//        $(By.xpath("//td[normalize-space()='State and City']/following-sibling::td[normalize-space()='NCR Delhi']"));
-
     }
+
 }
