6dd39ab808689f88633cdcfbadec0d2d21b22728
ALBERICH <Moroz.sergei94@gmail.com>
2026-03-03 22:44:22 +0300

    update upload

---
+++ src/test/java/tests/DownloadFile.java
@@ -0,0 +1,59 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.FileDownloadMode;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+
+public class DownloadFile {
+
+
+
+
+    @Test
+    void firstDowloadFile() throws Exception {
+
+        open("https://github.com/qa-guru/niffler/blob/master/README.md");
+        getWebDriver().manage().window().maximize();
+
+//        File download = $("button[data-testid='download-raw-button']").download();
+
+        Configuration.fileDownload = FileDownloadMode.FOLDER;
+        File download = $("button[data-testid='download-raw-button']").download();
+        try (InputStream is = new FileInputStream(download)) {
+            byte [] bytes = is.readAllBytes();
+            String fileAsString = new String(bytes, StandardCharsets.UTF_8);
+            Assertions.assertTrue(fileAsString.contains("Технологии, использованные в Niffler 2.0"));
+        }
+
+        Selenide.clearBrowserCookies();
+        Selenide.clearBrowserLocalStorage();
+    }
+
+
+    @Test
+    void secondDowloadFile() throws Exception {
+        Selenide.open("https://tus.io/demo.html");
+        getWebDriver().manage().window().maximize();
+        $("input[type='file']").uploadFromClasspath("img/BlueBird.jpg");
+        $(byXpath("//div[@class='_root_gq6c0_1']//p")).shouldHave(text("The upload is complete!"));
+        sleep(3000);
+    }
+
+}
