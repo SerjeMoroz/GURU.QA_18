3c3313518359442653f5331014a281298f320a15
ALBERICH <moroz.sergei94@gmail.com>
2026-04-28 17:16:42 +0300

    Обновил basetest(2)

--- src/test/java/examples/BaseTest.java
+++ src/test/java/examples/BaseTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chromium.ChromiumOptions;
 
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
@@ -21,8 +22,14 @@ public class BaseTest {
         options.addArguments("--disable-notifications");
         options.addArguments("--disable-search-engine-choice-screen");
         options.addArguments("--disable-features=OptimizationGuideModelDownloading,OptimizationHintsFetching,OptimizationTargetPrediction,OptimizationHints");
-        options.addArguments("--disable.cache");
+        options.addArguments("--disable-cache");
         options.addArguments("--ignore-certificate-errors");
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--remote-debugging-port=9222");
+        Configuration.browserCapabilities = options;
 //        options.addArguments("--unsafely-treat-insecure-origin-as-secure=хттпсайт");
         Configuration.browser = "chrome";
         Configuration.headless = true;
