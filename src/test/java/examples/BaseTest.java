ad258512d798aac7ad24a0bae313138ffcf1b38c
ALBERICH <Moroz.sergei94@gmail.com>
2026-05-08 22:47:06 +0300

    add allureListener and fix tests(5)

--- src/test/java/examples/BaseTest.java
+++ src/test/java/examples/BaseTest.java
@@ -45,14 +45,14 @@ public class BaseTest {
 //        Configuration.baseUrl = "https://demoqa.com";
         Configuration.fastSetValue = false;
 
-        Configuration.browserCapabilities = options;
-//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid.options", Map.<String, Object>of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
-        Configuration.browserCapabilities = capabilities;
+//        Configuration.browserCapabilities = options;
+////        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setCapability("selenoid.options", Map.<String, Object>of(
+//                "enableVNC", true,
+//                "enableVideo", true
+//        ));
+//        Configuration.browserCapabilities = capabilities;
     }
 
     @BeforeEach
