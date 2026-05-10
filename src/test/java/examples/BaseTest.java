040995114aa0c4b46a1c0aaab40a262d5e5eeeba
ALBERICH <Moroz.sergei94@gmail.com>
2026-05-10 21:52:46 +0300

    Удалил несколько тестовых классов и сделал удаленный запуск(2)

--- src/test/java/examples/BaseTest.java
+++ src/test/java/examples/BaseTest.java
@@ -38,19 +38,19 @@ public class BaseTest {
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--disable-gpu");
 //        options.addArguments("--headless=new");
-//        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--window-size=1920,1080");
 //        options.addArguments("--unsafely-treat-insecure-origin-as-secure=хттпсайт");
         Configuration.browser = "chrome";
         Configuration.baseUrl = "https://demoqa.com/";
         Configuration.fastSetValue = false;
 
-//        Configuration.browserCapabilities = options;
-//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-//        DesiredCapabilities capabilities = new DesiredCapabilities();
-//        capabilities.setCapability("selenoid.options", Map.<String, Object>of(
-//                "enableVNC", true,
-//                "enableVideo", true
-//        ));
+        Configuration.browserCapabilities = options;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid.options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
     }
 
     @BeforeEach
