6572d64e440ca929280b36c64359adc03ad7eb47
ALBERICH <Moroz.sergei94@gmail.com>
2026-05-10 23:00:13 +0300

    Прикрутил аттачи(2)

--- src/test/java/examples/BaseTest.java
+++ src/test/java/examples/BaseTest.java
@@ -3,6 +3,7 @@ package examples;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -61,6 +62,13 @@ public class BaseTest {
     }
 
     @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+
+    }
+
     void tearDown() {
         Selenide.clearBrowserCookies();
         Selenide.clearBrowserLocalStorage();
