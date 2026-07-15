0a990bc99800468a530216044d18288fabec001c
ALBERICH <moroz.sergei94@gmail.com>
2026-07-15 12:57:34 +0300

    Проверка JEnkins2

--- src/test/java/examples/BaseTest.java
+++ src/test/java/examples/BaseTest.java
@@ -46,7 +46,7 @@ public class BaseTest {
         Configuration.fastSetValue = false;
 
         Configuration.browserCapabilities = options;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid.options", Map.<String, Object>of(
                 "enableVNC", true,
