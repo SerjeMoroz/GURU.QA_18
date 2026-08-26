7e35df47926b7e31444084f86cf5b6e828a2f546
ALBERICH <Moroz.sergei94@gmail.com>
2026-08-27 00:50:41 +0300

    basetest (9.8)

--- src/test/java/examples/BaseTest.java
+++ src/test/java/examples/BaseTest.java
@@ -21,15 +21,15 @@ public class BaseTest {
     @BeforeAll
     static void setupClass() {
         ChromeOptions options = new ChromeOptions();
-//        options.addArguments(
-//                "--disable-notifications",
-//        "--disable-search-engine-choice-screen",
-//        "--disable-features=OptimizationGuideModelDownloading,OptimizationHintsFetching,OptimizationTargetPrediction,OptimizationHints",
-//        "--disable-cache",
-//        "--ignore-certificate-errors",
-//        "--no-sandbox", "--disable-dev-shm-usage"
-//      "--unsafely-treat-insecure-origin-as-secure=хттпсайт");
-//        );
+        options.addArguments(
+                "--disable-notifications",
+        "--disable-search-engine-choice-screen",
+        "--disable-features=OptimizationGuideModelDownloading,OptimizationHintsFetching,OptimizationTargetPrediction,OptimizationHints",
+        "--disable-cache",
+        "--ignore-certificate-errors",
+        "--no-sandbox", "--disable-dev-shm-usage"
+//      "--unsafely-treat-insecure-origin-as-secure=хттпсайт"
+        );
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability(ChromeOptions.CAPABILITY, options);
@@ -38,15 +38,6 @@ public class BaseTest {
                 "enableVideo", true
         ));
         Configuration.browserCapabilities = capabilities;
-//        options.setAcceptInsecureCerts(true);
-//        options.setPageLoadStrategy(PageLoadStrategy.EAGER);
-//
-//        options.setCapability("selenoid:options", Map.of(
-//                "enableVNC", true,
-//                "enableVideo", true
-//        ));
-
-//        Configuration.browserCapabilities = options;
 
         Configuration.remote = "https://qa_engineer:-aAb_-4gs53FD@selenoid.qa.guru/wd/hub";
         Configuration.browser = System.getProperty("browser", "chrome");
@@ -57,56 +48,6 @@ public class BaseTest {
         Configuration.headless = false;
 
     }
-//        Configuration.remote = System.getProperty("remote","https://qa_engineer:-aAb_-4gs53FD@selenoid.qa.guru/wd/hub");
-//        Configuration.browser = System.getProperty("browser", "chrome").toLowerCase();
-//        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-//        Configuration.browserVersion = System.getProperty("browserVersion", "151");
-//        Configuration.timeout = 15000;
-//        Configuration.pageLoadTimeout = 60000;
-//        Configuration.pageLoadStrategy = "eager";
-//
-//        String browserVersion = System.getProperty("browserVersion");
-//        if (browserVersion != null && !browserVersion.isBlank()) {
-//            Configuration.browserVersion = browserVersion;
-//        }
-//
-//        String remoteUrl = System.getProperty("remoteUrl");
-//        if (remoteUrl == null || remoteUrl.isBlank()) {
-//            String login = System.getProperty("remoteBrowserUrlLogin");
-//            String password = System.getProperty("remoteBrowserUrlPassword");
-//            if (login != null && password != null && !login.isBlank() && !password.isBlank()) {
-//                remoteUrl = "https://" + login + ":" + password + "@" +
-//                        System.getProperty("remoteBrowserUrl", "selenoid.autotests.cloud/wd/hub");
-//            }
-//        }
-//
-//        boolean remote = remoteUrl != null && !remoteUrl.isBlank();
-//        Configuration.headless = !remote && Boolean.parseBoolean(System.getProperty("headless", "false"));
-//
-//        if (remote) {
-//            Configuration.remote = remoteUrl;
-//            Configuration.browserCapabilities = selenoidCapabilities();
-//        }
-//    }
-//
-//    private static MutableCapabilities selenoidCapabilities() {
-//        Map<String, Object> selenoidOptions = Map.of(
-//                "enableVNC", true,
-//                "enableVideo", true
-//        );
-//        if ("chrome".equalsIgnoreCase(Configuration.browser)) {
-//            ChromeOptions options = new ChromeOptions();
-//            options.setAcceptInsecureCerts(true);
-//            options.setPageLoadStrategy(PageLoadStrategy.EAGER);
-//            options.addArguments("--no-sandbox", "--disable-dev-shm-usage");
-//            options.setCapability("selenoid:options", selenoidOptions);
-//            return options;
-//        }
-//        DesiredCapabilities capabilities = new DesiredCapabilities();
-//        capabilities.setCapability("pageLoadStrategy", "eager");
-//        capabilities.setCapability("selenoid:options", selenoidOptions);
-//        return capabilities;
-//    }
 
     @BeforeEach
     void setUp() {
